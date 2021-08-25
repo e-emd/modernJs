6ca75e6ea58685e7e96a70c56b27c54bda53db2a
e-emd <dewin.emd@gmail.com>
2021-08-24 22:04:51 -0600

    javascript refresher

---
+++ app.js
@@ -0,0 +1,13 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'hello'
+console.log(greeting);
+console.log([1, 4, 5]);
+console.log({ a: 1, b: 3, c: 4 });
+console.table({ a: 1, b: 3, c: 4 });
+
+console.clear();
+console.warn('This is warning');
+
